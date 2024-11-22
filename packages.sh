1f2c4a43df5131ce984e739197af3b305423fff0
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2024-11-22 23:20:57 +0100

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -6,10 +6,10 @@ sudo pacman -Syu
 sudo pacman -S vulkan-radeon lib32-vulkan-radeon libva-mesa-driver libva-utils lib32-mesa
 
 # Arch
-sudo pacman -S lutris alacritty flatpak dolphin-emu awesome pcmanfm nnn xclip audacity btop fastfetch discord zed github-cli picom steam paru neovim feh godot blender qbittorrent gimp vlc snes9x zsh eza lazygit otf-monaspace-nerd ttf-mona-sans zoxide bat rofi-calc
+sudo pacman -S alacritty flatpak dolphin-emu pcmanfm nnn audacity btop fastfetch discord zed github-cli steam paru neovim godot blender qbittorrent gimp vlc snes9x zsh eza lazygit otf-monaspace-nerd ttf-mona-sans zoxide bat
 
 # AUR
-yay -S bottles protonup-qt gruvbox-plus-icon-theme gruvbox-dark-gtk protontricks buku rofi-buku-git
+yay -S protonup-qt protontricks buku
 
 # Remove KDE bloat
 sudo pacman -R konsole dolphin haruna kate kcalc mpv dolphin-plugins mpvqt okular
