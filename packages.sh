dbb608e2fc7bd0566f288d8289f06a8eb8895ab2
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2024-11-24 00:25:19 +0100

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -6,7 +6,7 @@ sudo pacman -Syu
 sudo pacman -S vulkan-radeon lib32-vulkan-radeon libva-mesa-driver libva-utils lib32-mesa
 
 # Arch
-sudo pacman -S alacritty flatpak dolphin-emu pcmanfm nnn audacity btop fastfetch discord zed github-cli steam paru neovim godot blender qbittorrent gimp vlc snes9x zsh eza lazygit otf-monaspace-nerd ttf-mona-sans zoxide bat
+sudo pacman -S alacritty flatpak dolphin-emu pcmanfm obs-studio nnn audacity btop fastfetch discord zed github-cli steam paru neovim godot blender qbittorrent gimp vlc snes9x zsh eza lazygit otf-monaspace-nerd ttf-mona-sans zoxide bat
 
 # AUR
 yay -S protonup-qt protontricks buku
