62f01d0c9794e3948042da03218b23b3bca1e7d4
Wilmer Lindau <wilmer.lindau@gmail.com>
2025-06-25 15:38:05 +0200

    cleaned up script

--- packages.sh
+++ packages.sh
@@ -1,12 +1,73 @@
 #!/bin/bash
 
-sudo pacman -Syu
+# Exit immediately if a command exits with a non-zero status.
+set -e
 
-# Drivers
-sudo pacman -S vulkan-radeon lib32-vulkan-radeon libva-mesa-driver libva-utils lib32-mesa
+# Update system
+echo "Checking for updates..."
+sudo pacman -Syu --noconfirm
 
-# Arch
-sudo pacman -S alacritty flatpak dolphin-emu pcmanfm obs-studio nnn audacity btop fastfetch discord zed github-cli steam paru neovim godot blender qbittorrent gimp vlc snes9x zsh eza lazygit otf-monaspace-nerd ttf-mona-sans zoxide bat code pandoc-cli rocm-smi-lib uv radeontop hyprland uwsm libnewt rofi-wayland dunst xdg-desktop-portal-hyprland xdg-desktop-portal-gtk hyprpolkitagent qt5-wayland qt6-wayland
+# Installing drivers
+echo "Installing AMD graphics drivers..."
+sudo pacman -S --noconfirm \
+    vulkan-radeon \
+    lib32-vulkan-radeon \
+    libva-mesa-driver \
+    libva-utils \
+    lib32-mesa
 
-# AUR
-yay -S protonup-qt protontricks buku hyprls
+# Install packages
+echo "Installing packages (pacman)..."
+sudo pacman -S --noconfirm \
+    alacritty \
+    flatpak \
+    pcmanfm \
+    obs-studio \
+    nnn \
+    audacity \
+    btop \
+    fastfetch \
+    discord \
+    zed \
+    github-cli \
+    steam \
+    paru \
+    neovim \
+    godot \
+    blender \
+    qbittorrent \
+    gimp \
+    vlc \
+    snes9x \
+    zsh \
+    eza \
+    lazygit \
+    otf-monaspace-nerd \
+    ttf-mona-sans \
+    zoxide \
+    bat \
+    code \
+    pandoc-cli \
+    rocm-smi-lib \
+    uv \
+    radeontop \
+    hyprland \
+    uwsm \
+    libnewt \
+    rofi-wayland \
+    dunst \
+    xdg-desktop-portal-hyprland \
+    xdg-desktop-portal-gtk \
+    hyprpolkitagent \
+    qt5-wayland \
+    qt6-wayland \
+    hyprpaper
+
+echo "Installing packages (yay)..."
+yay -S --noconfirm \
+    protonup-qt \
+    protontricks \
+    buku \
+    hyprls
+
+echo "Script completed without errors"
